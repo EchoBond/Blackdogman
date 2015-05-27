c4b28b99972fed0dff363dc3ba115d3370e168b3
Blackdogman <superkidchy@qq.com>
2015-05-27 19:40:04 +0800

    v 0.1.9

--- src/com/speedbook/logAspect/LogAspect.java
+++ src/com/speedbook/logAspect/LogAspect.java
@@ -3,11 +3,9 @@ package com.speedbook.logAspect;
 import java.util.Date;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,22 +18,14 @@ public class LogAspect {
 	@Pointcut("execution(* com.speedbook.service.UserService.*(..))")
 	public void myPointcut(){}
 	
-	@Before(value="myPointcut()")
-	public void logBefore(){
-	}
-	@After(value="myPointcut()")
-	public void after(){
-	}
-	
 	@AfterReturning(value="myPointcut()",returning="result")
 	public void afterReturning(JoinPoint joinPoint, Object result){
 		String methodName = joinPoint.getSignature().getName();
-		String log="The method " + methodName + " ends with " + result;
-		System.out.println(log);
 		LoganderrorService logService=getLogService();
 		Loganderror loganderror=new Loganderror();
 		loganderror.setType("Log");
-		loganderror.setMethod(log);
+		loganderror.setMethod(methodName);
+		loganderror.setVaule(result.toString());
 		loganderror.setError("none");
 		loganderror.setDate(new Date().toString());
 		logService.AddLog(loganderror);
@@ -44,13 +34,12 @@ public class LogAspect {
 	@AfterThrowing(value="myPointcut()",throwing="e")
 	public void afterThrowing(JoinPoint joinPoint, Exception e){
 		String methodName = joinPoint.getSignature().getName();
-		String log="The method " + methodName;
-		String error="occurs excetion:" + e;
-		System.out.println(error);
+		String error=e.toString();
 		LoganderrorService logService=getLogService();
 		Loganderror loganderror=new Loganderror();
 		loganderror.setType("Error£¡");
-		loganderror.setMethod(log);
+		loganderror.setMethod(methodName);
+		loganderror.setVaule(e.toString());
 		loganderror.setError(error);
 		loganderror.setDate(new Date().toString());
 		logService.AddLog(loganderror);
